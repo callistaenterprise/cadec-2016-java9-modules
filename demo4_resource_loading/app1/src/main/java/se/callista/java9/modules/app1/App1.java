282fb451d502e96c7fd1a28922427039dcf9e18f
Håkan Dahl <hakan.dahl@gmail.com>
2016-01-14 10:16:53 +0100

    Added resource loading from URL.

--- demo4_resource_loading/app1/src/main/java/se/callista/java9/modules/app1/App1.java
+++ demo4_resource_loading/app1/src/main/java/se/callista/java9/modules/app1/App1.java
@@ -3,16 +3,29 @@ package se.callista.java9.modules.app1;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+import se.callista.java9.modules.common.api.Echo;
 
 public class App1 {
 
 	public static void main(String[] args) {
+		// make sure we have access to Echo in the common-lib module
+		Echo echo = new Echo();
+		echo.echo("echo still working!");
+		
 		// load resources
 		try {
 			App1 app = new App1();
 
 			app.load("app1.properties");
 			app.load("common.properties");
+			
+			app.tryLoadResourceAsUrl("app1.properties");
+			app.tryLoadResourceAsUrl("common.properties");
+			app.tryLoadResourceAsUrl("spring-context-example.xml");
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -31,6 +44,13 @@ public class App1 {
 
 	private byte[] loadResource(String name) throws IOException {
 		try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(name)) {
+		// also try *this* classloader ...
+		//try (InputStream is = getClass().getClassLoader().getResourceAsStream(name)) {
+			
+			if (is == null) {
+				System.out.println("Could not find resource!");
+			}
+			
 			byte[] buf = new byte[8192];
 			ByteArrayOutputStream bos = new ByteArrayOutputStream(buf.length);
 			int n = 0;
@@ -38,6 +58,17 @@ public class App1 {
 				bos.write(buf, 0, n);
 			}
 			return bos.toByteArray();
+		}
+	}
+	
+	private void tryLoadResourceAsUrl(String name) {
+		try {
+			Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources(name);
+			while (urls.hasMoreElements()) {
+				System.out.println("URL: " + urls.nextElement());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
